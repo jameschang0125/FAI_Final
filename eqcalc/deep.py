6cfeeccecd5f2accdf7ced14d276baaf0e6179d7
J.C <james@capoouo>
2023-06-09 17:29:28 +0800

    [DEBUG] add debug msg

--- eqcalc/deep.py
+++ eqcalc/deep.py
@@ -110,7 +110,7 @@ class GameTree():
         returns a dictionary
         '''
         if self.term: return self.signature
-        return ((self.signature, c.getTree()) for c in self.children)
+        return tuple((self.signature, c.getTree()) for c in self.children)
 
     def show(self, msg = None, suppress = True):
         eps = 1e-3
