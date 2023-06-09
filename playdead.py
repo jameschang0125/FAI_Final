be39239bf15438b593529114845a4ee99d57febc
J.C <james@capoouo>
2023-06-09 17:48:58 +0800

    [DEBUG] add debug msg

--- playdead.py
+++ playdead.py
@@ -4,7 +4,7 @@ from random import randint
 N = 100
 for i in range(N):
     try:
-        play(randint(0, len(ais) - 1), debug = True)
+        play(randint(0, len(ais) - 1))
     except Exception as e:
         print(e)
         exit()
